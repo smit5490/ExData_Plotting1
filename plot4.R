8aa2771ad387a8086d016a91ac50e30b5da70c22
smit5490 <smit5490@users.noreply.github.com>
2016-01-24 19:13:17 -0700

    final update

--- plot4.R
+++ plot4.R
@@ -15,5 +15,4 @@ with(subset,{
               legend("topright",bty="n",lty=c(1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
               plot(datetime,Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime")
   })
-#dev.copy(png,"plot4.png")
 dev.off()
